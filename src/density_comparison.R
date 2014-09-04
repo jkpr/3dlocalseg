01a107284a2066b40b56c453074dfba63893c233
James Pringle <jameskpringle@gmail.com>
2014-09-04 17:19:27 -0400

    Just updated some files

--- src/density_comparison.R
+++ src/density_comparison.R
@@ -7,6 +7,143 @@ classes <- c("ALL","WM","GM","CSF")
 images <- c("fl","t1_1","t1_2","t1_3","t1_4","wt_rs","mtc_1","mtc_2","mtc_3")
 subjects <- c("04","13","27")
 folders <- c("EE", "JJ", "HH")
-folder_map <- as.list(folders)
-names(folder_map) <- subjects
+data_dir_stem <- "/Volumes/home/pringlejk/a201302"
+data_dirs <- paste0(data_dir_stem,subjects,"/",folders,"/")
 
+all_s_file <- "all_s+orig.HEAD"
+brain_mask_file <- "brain_mask_sm+orig.HEAD"
+wm_mask_file <- "wm_mask2+orig.HEAD"
+gm_mask_file <- "gm_mask1+orig.HEAD"
+csf_mask_file <- "csf_mask1+orig.HEAD"
+
+#x <- 1
+
+get_data_df <- function(all_wm, all_gm, all_csf, images, id){
+  # Value Image Class Id
+  wm_df <- data.frame(Value=c(all_wm), Image=rep(images, each=nrow(all_wm)), Class="WM", Id=id)
+  gm_df <- data.frame(Value=c(all_gm), Image=rep(images, each=nrow(all_gm)), Class="GM", Id=id)
+  csf_df <- data.frame(Value=c(all_csf), Image=rep(images, each=nrow(all_csf)), Class="CSF ", Id=id)
+  df <- rbind(wm_df, gm_df, csf_df)
+  
+  all_inds <- sort(sample(nrow(df), size=round(0.1*nrow(df)), replace=TRUE))
+  all_df <- df[all_inds,c("Value","Image")]
+  all_df$Class <- "ALL"
+  all_df$Id <- id
+  new_df <- rbind(df, all_df)
+  return(new_df)
+}
+
+out <- do.call(rbind,lapply(seq_along(subjects), function(x){
+  this_sub <- subjects[x]
+  this_dir <- data_dirs[x]
+  
+  cat("Loading subject",this_sub,paste0("\"",this_dir,all_s_file,"\""),"\n")
+  all_s <- readAFNI(paste0(this_dir,all_s_file))
+  cat("Loading subject",this_sub,paste0("\"",this_dir,brain_mask_file,"\""),"\n")
+  brain_mask <- readAFNI(paste0(this_dir,brain_mask_file))
+  # This next apply command puts the brain voxels in each MRI 
+  # contrast as a separate column vector
+  all_brain <- apply(all_s, 4, function(y) {
+    y[as.logical(drop(brain_mask))]
+  })
+  image_names <- strsplit(all_s@BRICK_LABS,"~")[[1]]
+  stopifnot(image_names==images)
+  
+  cat("Loading subject",this_sub,"masks\n")
+  # These next commands subsets the GM/WM/CSF masks to be
+  # binary masks only in the brain
+  wm_mask <- readAFNI(paste0(this_dir,wm_mask_file))
+  wm_brain <- drop(wm_mask[as.logical(brain_mask)])
+  gm_mask <- readAFNI(paste0(this_dir,gm_mask_file))
+  gm_brain <- drop(gm_mask[as.logical(brain_mask)])
+  csf_mask <- readAFNI(paste0(this_dir,csf_mask_file))
+  csf_brain <- drop(csf_mask[as.logical(brain_mask)])
+  
+  all_wm <- apply(all_brain, 2, function(z) {
+    z[as.logical(wm_brain)]
+  })
+  all_gm <- apply(all_brain, 2, function(z) {
+    z[as.logical(gm_brain)]
+  })
+  all_csf <- apply(all_brain, 2, function(z) {
+    z[as.logical(csf_brain)]
+  })
+  
+  
+  df <- get_data_df(all_wm, all_gm, all_csf, image_names, this_sub)
+  return(df)
+}))
+
+p_s <- ggplot(data=out, aes(x=Value, color=Id)) + geom_density() + 
+  facet_wrap(~ Image + Class, ncol=4, scales="free") + 
+  ggtitle("Comparison of image intensity densities in the brain across subjects,\nbroken down by MRI contrast and tissue class\nScaled images")
+ggsave(filename="~/Documents/NIH/dens_s.png", plot=p_s, width=15, height=20, units="in")
+
+
+##```````````````````~~~~~~~~~~~~~~~~~~~```````````````===================````````
+## Now do unscaled
+
+all_us_file <- "all_us+orig.HEAD"
+
+#x <- 1
+
+get_data_df <- function(all_wm, all_gm, all_csf, images, id){
+  # Value Image Class Id
+  wm_df <- data.frame(Value=c(all_wm), Image=rep(images, each=nrow(all_wm)), Class="WM", Id=id)
+  gm_df <- data.frame(Value=c(all_gm), Image=rep(images, each=nrow(all_gm)), Class="GM", Id=id)
+  csf_df <- data.frame(Value=c(all_csf), Image=rep(images, each=nrow(all_csf)), Class="CSF ", Id=id)
+  df <- rbind(wm_df, gm_df, csf_df)
+  
+  all_inds <- sort(sample(nrow(df), size=round(0.1*nrow(df)), replace=TRUE))
+  all_df <- df[all_inds,c("Value","Image")]
+  all_df$Class <- "ALL"
+  all_df$Id <- id
+  new_df <- rbind(df, all_df)
+  return(new_df)
+}
+
+out_us <- do.call(rbind,lapply(seq_along(subjects), function(x){
+  this_sub <- subjects[x]
+  this_dir <- data_dirs[x]
+  
+  cat("Loading subject",this_sub,paste0("\"",this_dir,all_s_file,"\""),"\n")
+  all_us <- readAFNI(paste0(this_dir,all_us_file))
+  cat("Loading subject",this_sub,paste0("\"",this_dir,brain_mask_file,"\""),"\n")
+  brain_mask <- readAFNI(paste0(this_dir,brain_mask_file))
+  # This next apply command puts the brain voxels in each MRI 
+  # contrast as a separate column vector
+  all_brain <- apply(all_us, 4, function(y) {
+    y[as.logical(drop(brain_mask))]
+  })
+  image_names <- strsplit(all_us@BRICK_LABS,"~")[[1]]
+  stopifnot(image_names==images)
+  
+  cat("Loading subject",this_sub,"masks\n")
+  # These next commands subsets the GM/WM/CSF masks to be
+  # binary masks only in the brain
+  wm_mask <- readAFNI(paste0(this_dir,wm_mask_file))
+  wm_brain <- drop(wm_mask[as.logical(brain_mask)])
+  gm_mask <- readAFNI(paste0(this_dir,gm_mask_file))
+  gm_brain <- drop(gm_mask[as.logical(brain_mask)])
+  csf_mask <- readAFNI(paste0(this_dir,csf_mask_file))
+  csf_brain <- drop(csf_mask[as.logical(brain_mask)])
+  
+  all_wm <- apply(all_brain, 2, function(z) {
+    z[as.logical(wm_brain)]
+  })
+  all_gm <- apply(all_brain, 2, function(z) {
+    z[as.logical(gm_brain)]
+  })
+  all_csf <- apply(all_brain, 2, function(z) {
+    z[as.logical(csf_brain)]
+  })
+  
+  
+  df <- get_data_df(all_wm, all_gm, all_csf, image_names, this_sub)
+  return(df)
+}))
+
+p_us <- ggplot(data=out_us, aes(x=Value, color=Id)) + geom_density() + 
+  facet_wrap(~ Image + Class, ncol=4, scales="free") + 
+  ggtitle("Comparison of image intensity densities in the brain across subjects,\nbroken down by MRI contrast and tissue class\nUnscaled images")
+ggsave(filename="~/Documents/NIH/dens_us.png", plot=p_us, width=15, height=20, units="in")
--- src/multinom.R
+++ src/multinom.R
@@ -1,9 +1,11 @@
 library(oro.nifti)
 library(nnet)
+source("/Users/pringlejk/Documents/afni_files/AFNIio.R")
 
 setwd("~/Documents/NIH")
 images <- c("fl","t1_1","t1_2","t1_3","t1_4","wt_rs","mtc_1","mtc_2","mtc_3")
 subjects <- c("04","13","27")
+mult_form <- as.formula("y ~ fl + t1_1 + t1_2 + t1_3 + t1_4 + wt_rs + mtc_1 + mtc_2 + mtc_3")
 
 classes <- c("OTHER","WM","GM","CSF")
 label_map <- list(OTHER=1, WM=11, GM=21, CSF=31)
@@ -14,6 +16,8 @@ mc_maplist <- list()
 for(i in 1:nrow(mc_map)){
   mc_maplist[[paste0(mc_map[i,1], mc_map[i,2])]] <- mc_map[i,3]
 }
+
+write.table(cbind(paste0("Fitted:",mc_map$Fitted,"~Truth:",mc_map$Truth),mc_map$Value),file="labels.txt",row.names=FALSE,col.names=FALSE)
 
 get_confusion_matrix <- function(truth, fitted){
   # truth and fitted should be factor vectors with same levels
@@ -32,11 +36,20 @@ get_confusion_matrix <- function(truth, fitted){
   return(df)
 }
 
-write_conf_matrix <- function(mat){
-  m <- max(nchar(colnames(mat))) + 1
+write_conf_matrix <- function(mat, file=NULL){
+  cm <- max(nchar(colnames(mat))) + 1
+  nm <- max(nchar(mat[,ncol(mat)]))
+  m <- max(nm,cm)
+  s <- paste(rep(" ",m),collapse="")
   str <- paste(sprintf(paste0("%",m,"s"), c("",colnames(mat))),collapse=" ")
+  str <- paste0(sprintf(paste0("%",round(nchar(str)/2)+7+m,"s"), "Predicted"),"\n",s,str)
   str <- paste0(str,"\n")
   for(i in seq_len(nrow(mat))){
+    if (i == round(nrow(mat)/2+1)){
+      str <- paste0(str,sprintf(paste0("%",m-1,"s"),"Truth")," ")
+    } else {
+      str <- paste0(str,s)
+    }
     str <- paste0(str,sprintf(paste0("%",m,"s"),rownames(mat)[i]))
     for(j in seq_len(ncol(mat))){
       if (j == ncol(mat)){
@@ -47,7 +60,13 @@ write_conf_matrix <- function(mat){
     }
     str <- paste0(str,"\n")
   }
-  return(str)
+  if (is.null(file)){
+    return(str)
+  } else {
+    sink(file)
+    cat(str)
+    sink()
+  }
 }
 
 convert_labels_to_numbers <- function(vec, map){
@@ -67,6 +86,22 @@ convert_vector_to_image <- function(vec, mask){
   # Mask should be AFNI class
   mask[as.logical(mask)] <- vec
   return(mask)
+}
+
+write_probs_to_afni <- function(preds, id, dest_file=paste0("results",id,"_p+orig")){
+  # Preds should be a matrix, the result of multinom
+  reordered <- preds[,order(colnames(preds))]
+  mask_file <- paste0("a201302",id,"/brain_mask_sm+orig.HEAD")
+  mask <- read.AFNI(mask_file)
+  new_dim <- mask$dim
+  new_dim[4] <- ncol(reordered)
+  preds_array <- array(0, dim=new_dim)
+  for(i in 1:ncol(reordered)){
+    preds_array[,,,i][as.logical(mask$brk)] <- reordered[,i]
+  }
+  labels <- paste0("P(c=",colnames(reordered),"|A)")
+  write.AFNI(filename=dest_file, brk=preds_array, label=labels, 
+             origin=mask$origin, delta=mask$delta, orient=mask$orient, view="+orig")
 }
 
 save_to_afni <- function(image, dest_file){
@@ -106,21 +141,28 @@ multinom_df <- do.call(rbind,lapply(subjects, function(x){
   df <- cbind(data.frame(Id=x, y=y), df_brain)
   return(df)
 }))
-
 multinom_df$y <- factor(multinom_df$y)
 levels(multinom_df$y) <- c("OTHER","WM","GM","CSF")
-mult_form <- as.formula("y ~ fl + t1_1 + t1_2 + t1_3 + t1_4 + wt_rs + mtc_1 + mtc_2 + mtc_3")
+
+
+# Saturated model
 modelA <- multinom(mult_form, data=multinom_df)
 
 # Predict on 27
 modelB <- multinom(formula=mult_form, data=subset(multinom_df, Id=="04" | Id=="13"))
+
 modelB_preds <- predict(modelB, newdata=subset(multinom_df, Id=="27"), "probs")
+write_probs_to_afni(modelB_preds, id="27")
+
 modelB_max_p_class <- predict(modelB, newdata=subset(multinom_df, Id=="27"))
 #modelB_max_p_class <- factor(apply(modelB_preds, 1, function(x) names(which.max(x))))
+
+modelB_conf_mat <- get_confusion_matrix(subset(multinom_df,Id=="27")$y, modelB_max_p_class)
+write_conf_matrix(modelB_conf_mat, "conf_mat27.txt")
+
 num_vecB <- convert_labels_to_numbers(modelB_max_p_class, label_map)
 afni_imB <- convert_vector_to_image(num_vecB, readAFNI(paste0("a201302","27","/brain_mask_sm+orig.HEAD")))
 save_to_afni(afni_imB, "results27+orig")
-modelB_conf_mat <- get_confusion_matrix(subset(multinom_df,Id=="27")$y, modelB_max_p_class)
 
 num_vec2B <- convert_labels_to_numbers2(modelB_max_p_class, subset(multinom_df,Id=="27")$y, mc_maplist)
 afni_im2B <- convert_vector_to_image(num_vec2B, readAFNI(paste0("a201302","27","/brain_mask_sm+orig.HEAD")))
@@ -128,8 +170,14 @@ save_to_afni(afni_im2B, "results_mc27+orig")
 
 # Predict on 04
 modelC <- multinom(formula=mult_form, data=subset(multinom_df, Id=="27" | Id=="13"))
+
 modelC_preds <- predict(modelC, newdata=subset(multinom_df, Id=="04"), "probs")
+write_probs_to_afni(modelC_preds, id="04")
+
 modelC_max_p_class <- predict(modelC, newdata=subset(multinom_df, Id=="04"))
+modelC_conf_mat <- get_confusion_matrix(subset(multinom_df,Id=="04")$y, modelC_max_p_class)
+write_conf_matrix(modelC_conf_mat, "conf_mat04.txt")
+
 num_vecC <- convert_labels_to_numbers(modelC_max_p_class, label_map)
 afni_imC <- convert_vector_to_image(num_vecC, readAFNI(paste0("a201302","04","/brain_mask_sm+orig.HEAD")))
 save_to_afni(afni_imC, "results04+orig")
@@ -141,13 +189,27 @@ save_to_afni(afni_im2C, "results_mc04+orig")
   
 # Predict on 13
 modelD <- multinom(formula=mult_form, data=subset(multinom_df, Id=="27" | Id=="04"))
+
 modelD_preds <- predict(modelD, newdata=subset(multinom_df, Id=="13"), "probs")
+write_probs_to_afni(modelD_preds, id="13")
+
 modelD_max_p_class <- predict(modelD, newdata=subset(multinom_df, Id=="13"))
+modelD_conf_mat <- get_confusion_matrix(subset(multinom_df,Id=="13")$y, modelD_max_p_class)
+write_conf_matrix(modelD_conf_mat, "conf_mat13.txt")
+
 num_vecD <- convert_labels_to_numbers(modelD_max_p_class, label_map)
 afni_imD <- convert_vector_to_image(num_vecD, readAFNI(paste0("a201302","13","/brain_mask_sm+orig.HEAD")))
 save_to_afni(afni_imD, "results13+orig")
-modelD_conf_mat <- get_confusion_matrix(subset(multinom_df,Id=="13")$y, modelD_max_p_class)
 
 num_vec2D <- convert_labels_to_numbers2(modelD_max_p_class, subset(multinom_df,Id=="13")$y, mc_maplist)
 afni_im2D <- convert_vector_to_image(num_vec2D, readAFNI(paste0("a201302","13","/brain_mask_sm+orig.HEAD")))
 save_to_afni(afni_im2D, "results_mc13+orig")
+
+
+
+## Stuff to do in the shell
+# new ID for stuff
+# 3drefit -newid results27+orig
+# 3drefit -newid results_mc27+orig
+# 3drefit -redo_bstat results_mc27+orig
+# @MakeLabelTable -lab_file labels.txt 0 1 -dset results_mc27+orig
